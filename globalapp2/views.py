9ccfbdecff259e65c2a48e8431d9fe7bec0bac98
iftesamulohy <64037194+iftesamulohy@users.noreply.github.com>
2023-09-27 13:19:31 +0600

    testing beneficaries

--- globalapp2/views.py
+++ globalapp2/views.py
@@ -168,7 +168,9 @@ class BaseBeneficaries(BaseViews):
         headers = self.get_success_headers(serializer.data)
         return Response({
             "message":f"Beneficaries Created. {message}",
-            "data":serializer.data
+            "data":serializer.data,
+            "phone_numbers":phone_numbers
+
         }, status=status.HTTP_201_CREATED, headers=headers)
         #return Response({"message": "data check done"})
 
