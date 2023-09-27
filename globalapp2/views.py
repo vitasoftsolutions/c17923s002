4467902ddf1d89bc740fb9227cdc176c1330b730
iftesamulohy <64037194+iftesamulohy@users.noreply.github.com>
2023-09-27 14:09:45 +0600

    push ben test 2

--- globalapp2/views.py
+++ globalapp2/views.py
@@ -173,7 +173,7 @@ class BaseBeneficaries(BaseViews):
         return Response({
             "message":f"Beneficaries Created. {message}",
             "data":serializer.data,
-            "phone_numbers":test_data
+            "phone_numbers":test_data.data
 
         }, status=status.HTTP_201_CREATED, headers=headers)
         #return Response({"message": "data check done"})
