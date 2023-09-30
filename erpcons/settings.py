bbb46141456d62f3e152d506001642fb8ea6a6a8
iftesamulohy <64037194+iftesamulohy@users.noreply.github.com>
2023-09-30 12:53:31 +0600

    employee phone module updated 2

--- erpcons/settings.py
+++ erpcons/settings.py
@@ -15,7 +15,7 @@ import os
 #import dj_database_url
 from datetime import timedelta
 #from decouple import config 
-HOST=False
+HOST=True
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
