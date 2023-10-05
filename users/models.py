2bdb13478800ca297921a772f309a62f932fca41
iftesamulohy <64037194+iftesamulohy@users.noreply.github.com>
2023-10-05 11:27:31 +0600

    NID case issue solved

--- users/models.py
+++ users/models.py
@@ -46,7 +46,7 @@ class Employee(AbstractBaseUser):
     #phone_number = models.ManyToManyField(PhoneNumber)
     present_address = models.TextField()
     permanent_address = models.TextField(null=True)
-    NID_number = models.CharField(max_length=20)
+    nid_number = models.CharField(max_length=20)
     nid_front = models.FileField(
         upload_to="nid/",
         verbose_name='NID Image (Front)',
